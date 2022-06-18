699eac826dee9db4ca14fd4521da45bb780f9ee8
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-18 22:19:18 +0800

    完成DCB样式

--- src/components/DCB/DCB.js
+++ src/components/DCB/DCB.js
@@ -5,6 +5,11 @@ export default function DCB() {
   return (
     <div className='dcb-container'>
       <img className='dcb-bg' src={process.env.PUBLIC_URL + '/img/dcb_bg.png '} alt='Daddy, Come Back! background' />
+      <h2 className='dcb-welcome'>
+        Welcome to
+        <br />
+        Rug Pull Games
+      </h2>
     </div>
   );
 }
