b91771e10277d939320683fdb9fe3c91483a9f94
Yuancheng Zhang <old.far.hot@gmail.com>
2022-06-19 02:21:19 +0800

    添加整平滚动

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import './App.css';
+import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage';
 import Logo from './components/Logo';
 import DCB from './components/DCB';
 import Projects from './components/Projects';
+import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Logo />
-      <DCB />
-      <Projects />
+      <Fullpage>
+        <FullPageSections>
+          <FullpageSection>
+            <Logo />
+          </FullpageSection>
+          <FullpageSection>
+            <DCB />
+          </FullpageSection>
+          <FullpageSection>
+            <Projects />
+          </FullpageSection>
+        </FullPageSections>
+      </Fullpage>
     </div>
   );
 }
